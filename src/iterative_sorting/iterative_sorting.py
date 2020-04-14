00fe9189249fac4d857011d39b393dd4793263c7
stackblock <lambdaTL.anthony@gmail.com>
2020-04-14 10:59:28 -0400

    complete day 1 assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,51 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+
+import random
+list = random.sample(range(1, 20), 10)
+
+# list = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        for number in range(cur_index, len(arr)):
+            if arr[number] < arr[smallest_index]:
+               smallest_index = number
+              # TO-DO: swap
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        # (hint, can do in 3 loc)
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    changes = True
+    while changes:
+        for i in range(1, len(arr)):
+            # print(arr)
+            changes = False
+            if arr[i - 1] > arr[i]:
+                # print("yes")
+                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+                # print(arr)
+                changes = True
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
+def count_sort(arr, maximum=-1):
     return arr
+
+
+NewList = list
+
+print(f'Original list: {NewList}')
+
+print(f'Selection Sort: {selection_sort(NewList)}')
+print(f'Bubble Sort: {bubble_sort(NewList)}')
